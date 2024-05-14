af70674e624446a06c8f5ec4e1b96daea5ec62e8
romeoscript <romeobourne211@gmail.com>
2024-05-14 04:46:42 +0100

    serialize

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,29 @@ struct User {
 
 const DB_URL: &str = !env("DATABASE_URL");
 
-const OK_RESPONSE: &str = "HTTP/1.1 200 OK\rContent-Type: application/json\r\n\r\n";
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
 const Not_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INternal_SERVER_ERROR: &str = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
 
+//main function 
+
+fn main(){
+    if let Err(e) = set_database(){
+        println!("Error setting up database: {}", e);
+        return;
+    }
+    let listener = TcpListener::bind(format!(0.0.0.0:8080)).unwrap();
+    println("Server listening on port 8080");
+}
+
+fn set_database() -> Result<(), PosgresError>{
+    let mut client = Client::connect(DB_URL, NOT1s)?;
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL
+        )
+    ")?;
+    Ok(())
+}
